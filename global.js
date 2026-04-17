8e3776f90567d6e161e9eb57eee3bac5fefc242c
pemam238 <pemam@ucsd.edu>
2026-04-17 09:14:16 -0700

    Completed w/o step 5

---
+++ global.js
@@ -0,0 +1,78 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/pemam238', title: 'Github' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/pemam238/";         // GitHub Pages repo name
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  // next step: create link and add it to nav
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+
+ if (a.host === location.host && a.pathname === location.pathname) {
+  a.classList.add('current');
+ }
+
+ if (a.host !== location.host){
+    a.target = "_blank";
+ }
+ nav.append(a);
+}
+
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+`
+);
+
+let select = document.querySelector('.color-scheme select');
+
+
+function setColorScheme(colorScheme) {
+  document.documentElement.style.setProperty('color-scheme', colorScheme);
+  select.value = colorScheme;
+}
+
+
+if ('colorScheme' in localStorage) {
+  setColorScheme(localStorage.colorScheme);
+}
+
+
+select.addEventListener('input', function (event) {
+ setColorScheme(event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
+
